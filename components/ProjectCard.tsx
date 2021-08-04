76d4fae2f964132ca422cfa9ca77f306dac83404
oliverloops <oliveralr@protonmail.com>
2021-08-04 13:45:16 -0500

    Adding card form

--- components/ProjectCard.tsx
+++ components/ProjectCard.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function ProjectCard() {
   return (
-    <div className="bg-white shadow-md rounded-lg md:w-72">
+    <div className="bg-white shadow-md rounded-lg md:w-80">
       <Image
         className="rounded-t-lg"
         width={370}
@@ -16,6 +16,27 @@ export default function ProjectCard() {
           Cumplimiento Ambiental: <span className="font-bold">X</span>
         </p>
       </span>
+      <form className="flex flex-col px-4 py-2 pb-4">
+        <label className="text-sm py-1">Usuario</label>
+        <input
+          type="text"
+          className="border-2 border-gray-300 rounded text-sm px-1 py-0.5"
+          placeholder="Ingresa tu nombre de usuario"
+        />
+        <label className="text-sm py-1">Contraseña</label>
+        <input
+          type="password"
+          className="border-2 border-gray-300 rounded text-sm px-1 py-0.5"
+          placeholder="Ingresa tu contraseña"
+        />
+      </form>
+      <div className="w-full h-px bg-gray-200"></div>
+      <p
+        style={{ color: "#8CBA6E" }}
+        className="cursor-pointer text-center text-xs underline p-2"
+      >
+        Click para abrir formulario
+      </p>
     </div>
   );
 }
