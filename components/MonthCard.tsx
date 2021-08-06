b8b3cacb028030a3f59754944ba8873d0a41eef8
oliverloops <oliveralr@protonmail.com>
2021-08-06 14:10:07 -0500

    Adding months page layout

--- components/MonthCard.tsx
+++ components/MonthCard.tsx
@@ -1,7 +1,21 @@
-export default function MonthCard() {
+import Link from "next/link";
+import Image from "next/image";
+
+export default function MonthCard(props: { month: number; route: string }) {
   return (
-    <div className="bg-white shadow-md rounded-lg">
-      <p>Some text</p>
-    </div>
+    <Link href={props.route}>
+      <a>
+        <div className="bg-white shadow-md rounded-xl">
+          <Image
+            className="rounded-t-xl"
+            src={"/picachos.jpg"}
+            width={260}
+            height={68}
+            alt="picachos banner"
+          />
+          <p className="">Mes {props.month}</p>
+        </div>
+      </a>
+    </Link>
   );
 }
