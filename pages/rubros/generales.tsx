0c4d5b2e4a0190b8239b8f1b5f227a02687e1c12
oliverloops <oliveralr@protonmail.com>
2021-08-08 15:47:53 -0500

    Adding generales page layout

--- pages/rubros/generales.tsx
+++ pages/rubros/generales.tsx
@@ -1,13 +1,56 @@
+import { RiSendPlaneFill } from "react-icons/ri";
 //UI Layout
 import Header from "../../layout/Header";
 import Footer from "../../layout/Footer";
+//UI Components
+import Button from "../../components/Button";
 
 export default function Generales() {
   return (
     <>
       <Header />
-      <h1>Generales</h1>
+      <div className="flex justify-between  md:w-2/6 p-4 md:p-8">
+        <Button text={"Regresar"} route={"/rubros/select"} arrow={"left"} />
+        <Button text={"Rubros"} route={"#"} arrow={""} />
+        <Button text={"Continuar"} route={"#"} arrow={"right"} />
+      </div>
+      <Content />
       <Footer />
     </>
+  );
+}
+
+function Content() {
+  return (
+    <form className="flex flex-col p-8">
+      <label className="text-xl font-semibold py-2">
+        Cátalogo General de Obra
+      </label>
+      <select className="border-2 bg-gray-200 border-gray-300 rounded-md w-40 h-10 px-3 py-0.5">
+        <option>V1</option>
+        <option>V2</option>
+        <option>V3</option>
+        <option>v4</option>
+        <option>V5</option>
+      </select>
+      <label className="text-xl font-semibold py-4">Área</label>
+      <select className="border-2 bg-gray-200 border-gray-300 rounded-md  w-40 h-10 px-3 py-0.5">
+        <option>Frente 1</option>
+        <option>Frente 2</option>
+        <option>Frente 3</option>
+        <option>Frente 4</option>
+        <option>Frente 5</option>
+      </select>
+      <label className="text-xl font-semibold py-4">
+        Coordenadas UTM Centroíde
+      </label>
+      <select className="border-2 bg-gray-200 border-gray-300 rounded-md  w-40 h-10 px-3 py-0.5">
+        <option>Frente 1</option>
+        <option>Frente 2</option>
+        <option>Frente 3</option>
+        <option>Frente 4</option>
+        <option>Frente 5</option>
+      </select>
+    </form>
   );
 }
