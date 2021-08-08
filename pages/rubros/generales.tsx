7df539c7499d1557d67efbd1688d0d084cb46b61
oliverloops <oliveralr@protonmail.com>
2021-08-08 15:49:57 -0500

    Adding generales page layout

--- pages/rubros/generales.tsx
+++ pages/rubros/generales.tsx
@@ -23,28 +23,28 @@ export default function Generales() {
 function Content() {
   return (
     <form className="flex flex-col p-8">
-      <label className="text-xl font-semibold py-2">
+      <label className="text-xl font-semibold py-8 md:py-4">
         Cátalogo General de Obra
       </label>
-      <select className="border-2 bg-gray-200 border-gray-300 rounded-md w-40 h-10 px-3 py-0.5">
+      <select className="border-2 bg-gray-200 border-gray-300 rounded-md w-40 h-12 md:h-10 px-3 py-0.5">
         <option>V1</option>
         <option>V2</option>
         <option>V3</option>
         <option>v4</option>
         <option>V5</option>
       </select>
-      <label className="text-xl font-semibold py-4">Área</label>
-      <select className="border-2 bg-gray-200 border-gray-300 rounded-md  w-40 h-10 px-3 py-0.5">
+      <label className="text-xl font-semibold py-8 md:py-4">Área</label>
+      <select className="border-2 bg-gray-200 border-gray-300 rounded-md  w-40 h-12 md:h-10 px-3 py-0.5">
         <option>Frente 1</option>
         <option>Frente 2</option>
         <option>Frente 3</option>
         <option>Frente 4</option>
         <option>Frente 5</option>
       </select>
-      <label className="text-xl font-semibold py-4">
+      <label className="text-xl font-semibold py-8 md:py-4">
         Coordenadas UTM Centroíde
       </label>
-      <select className="border-2 bg-gray-200 border-gray-300 rounded-md  w-40 h-10 px-3 py-0.5">
+      <select className="border-2 bg-gray-200 border-gray-300 rounded-md  w-40 h-12 md:h-10 px-3 py-0.5">
         <option>Frente 1</option>
         <option>Frente 2</option>
         <option>Frente 3</option>
