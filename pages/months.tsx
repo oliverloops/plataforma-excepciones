0c3a2e47811195ff366d13224b7256bce2455f5d
oliverloops <oliveralr@protonmail.com>
2021-08-28 02:06:34 -0500

    Adding protected routes

--- pages/months.tsx
+++ pages/months.tsx
@@ -6,8 +6,6 @@ import Button from "../components/Button";
 import MonthCard from "../components/MonthCard";
 
 export default function Months({ data }) {
-  console.log(data);
-
   return (
     <>
       <Header />
@@ -15,23 +13,14 @@ export default function Months({ data }) {
         <Button text={"Ir a Inicio"} route={"/"} arrow={"left"} />
       </div>
       <div className="grid grid-cols-2 md:grid-cols-4 grid-rows-auto justify-items-center gap-y-8 px-4 md:px-8 py-8">
-        <MonthCard
-          month={1}
-          ica={2}
-          route={"/rubros/select"}
-          status={"Autorizado"}
-        />
-        <MonthCard month={2} ica={2} route={"#"} status={"No Autorizado"} />
-        <MonthCard month={3} ica={2} route={"#"} status={"Autorizado"} />
-        <MonthCard month={4} ica={2} route={"#"} status={"En Proceso"} />
-        <MonthCard month={5} ica={2} route={"#"} status={"Autorizado"} />
-        <MonthCard month={6} ica={2} route={"#"} status={"Autorizado"} />
-        <MonthCard month={7} ica={2} route={"#"} status={"En Proceso"} />
-        <MonthCard month={8} ica={2} route={"#"} status={"No Autorizado"} />
-        <MonthCard month={9} ica={2} route={"#"} status={"No Autorizado"} />
-        <MonthCard month={10} ica={2} route={"#"} status={"Autorizado"} />
-        <MonthCard month={11} ica={2} route={"#"} status={"Autorizado"} />
-        <MonthCard month={12} ica={2} route={"#"} status={"En Proceso"} />
+        {data.map((elem, id) => (
+          <MonthCard
+            month={id + 1}
+            ica={elem.ica}
+            route={"/rubros/select"}
+            status={"Autorizado"}
+          />
+        ))}
       </div>
       <Footer />
     </>
