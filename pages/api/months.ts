9b458c1c4a60d937a433f102f79c27eb50abb347
oliverloops <oliveralr@protonmail.com>
2021-09-03 00:45:24 -0500

    Updating db schema

--- pages/api/months.ts
+++ pages/api/months.ts
@@ -5,11 +5,16 @@ const connection = mysql.createConnection(process.env.DATABASE_URL);
 connection.connect();
 
 export default async function (req: NextApiRequest, res: NextApiResponse) {
+  console.log(req.body);
+
   switch (req.method) {
-    case "GET":
-      connection.query("SELECT * FROM months", (err, rows, fields) => {
-        res.send(rows);
-      });
+    case "POST":
+      connection.query(
+        `SELECT * FROM months WHERE project_title='${req.body}'`,
+        (err, rows, fields) => {
+          res.send(rows);
+        }
+      );
       break;
     default:
       res.status(405).end();
--- pages/months.tsx
+++ pages/months.tsx
@@ -1,3 +1,4 @@
+import { useState, useLayoutEffect } from "react";
 import { useRouter } from "next/router";
 //UI Layout
 import Header from "../layout/Header";
@@ -5,9 +6,22 @@ import Footer from "../layout/Footer";
 //UI Components
 import Button from "../components/Button";
 import MonthCard from "../components/MonthCard";
+import KilaLoader from "../components/KilaLoader";
 
-function Months({ data }) {
+function Months() {
+  const [months, setMonths] = useState(null);
+
   const router = useRouter();
+
+  useLayoutEffect(() => {
+    fetch("http://localhost:3000/api/months", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(router.query.project_title),
+    })
+      .then((res) => res.json())
+      .then((data) => setMonths(data));
+  }, []);
 
   return (
     <>
@@ -16,16 +30,22 @@ function Months({ data }) {
         <Button text={"Ir a Inicio"} route={"/"} arrow={"left"} />
       </div>
       <div className="grid grid-cols-2 md:grid-cols-4 grid-rows-auto justify-items-center gap-y-8 px-4 md:px-8 py-8">
-        {data.map((elem, id) => (
-          <MonthCard
-            key={id}
-            month={id + 1}
-            ica={elem.ica}
-            route={"/rubros/select"}
-            status={"Autorizado"}
-            query={JSON.stringify(router.query)}
-          />
-        ))}
+        {months === null ? (
+          <div>
+            <KilaLoader />
+          </div>
+        ) : (
+          months.map((elem, id) => (
+            <MonthCard
+              key={id}
+              month={id + 1}
+              ica={elem.ica}
+              route={"/rubros/select"}
+              status={"Autorizado"}
+              query={JSON.stringify(router.query)}
+            />
+          ))
+        )}
       </div>
       <Footer />
     </>
@@ -33,14 +53,3 @@ function Months({ data }) {
 }
 
 export default Months;
-
-export async function getStaticProps() {
-  const res = await fetch("http://localhost:3000/api/months");
-  const data = await res.json();
-
-  return {
-    props: {
-      data,
-    },
-  };
-}
