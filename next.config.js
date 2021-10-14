859b2570c4540149da3c72f0b7e104d1d71cbbca
oliverloops <oliveralr@protonmail.com>
2021-10-13 20:12:07 -0500

    env variable added

--- next.config.js
+++ next.config.js
@@ -1,11 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-  webpack: (config, { isServer, webpack }) => {
-    config.plugins.push(
-      new webpack.IgnorePlugin({
-        resourceRegExp: /^cardinal$/,
-        contextRegExp: /./,
-      })
-    );
-  },
 };
